3dcbb1f237348b84c2c941868dc27e96aeed0380
pon <kunbu0203@gmail.com>
2024-09-29 15:03:07 +0800

    video test

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -45,7 +45,7 @@ $(async function () {
         streamObj.getTracks().forEach(track => track.stop());
         front = !front;
         openCam();
-        // restartMediaPipeCamera(); // 重新啟動 MediaPipe Camera
+        restartMediaPipeCamera(); // 重新啟動 MediaPipe Camera
     });
 
 
@@ -77,49 +77,49 @@ $(async function () {
             }
             ctx.drawImage(results.image, 0, 0, $canvas.width, $canvas.height);
 
-            if (results.multiFaceLandmarks) {
-                for (const landmarks of results.multiFaceLandmarks) {
-                    // drawConnectors(ctx, landmarks, FACEMESH_TESSELATION,
-                    //     { color: '#C0C0C070', lineWidth: 1 });
-                    drawConnectors(ctx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
-                    drawConnectors(ctx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
-                    drawConnectors(ctx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
-                    drawConnectors(ctx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
-                }
-            }
-
-            // if (results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
-            //     const landmarks = results.multiFaceLandmarks[0];
-
-            //     // 計算臉部的外接矩形範圍
-            //     let minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
-            //     for (const point of landmarks) {
-            //         const x = point.x * $canvas.width;
-            //         const y = point.y * $canvas.height;
-            //         minX = Math.min(minX, x);
-            //         minY = Math.min(minY, y);
-            //         maxX = Math.max(maxX, x);
-            //         maxY = Math.max(maxY, y);
+            // if (results.multiFaceLandmarks) {
+            //     for (const landmarks of results.multiFaceLandmarks) {
+            //         // drawConnectors(ctx, landmarks, FACEMESH_TESSELATION,
+            //         //     { color: '#C0C0C070', lineWidth: 1 });
+            //         drawConnectors(ctx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
+            //         drawConnectors(ctx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
+            //         drawConnectors(ctx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
+            //         drawConnectors(ctx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
             //     }
-
-            //     // 計算臉部面積
-            //     const faceWidth = maxX - minX;
-            //     const faceHeight = maxY - minY;
-            //     const faceArea = faceWidth * faceHeight;
-
-            //     // 根據面積調整圖片大小，面積越大，頭越近
-            //     const scale = faceArea / (window.innerWidth * window.innerHeight);
-            //     // $('.text').text(scale);
-
-            //     // 計算頭頂的座標
-            //     const topOfHead = landmarks[10];
-            //     const x = topOfHead.x * $canvas.width;
-            //     const y = topOfHead.y * $canvas.height;
-
-            //     const imgW = faceWidth + (scale * 10);
-            //     // 根據比例繪製圖片
-            //     ctx.drawImage(img, x - imgW / 2, y - imgW, imgW, imgW);
             // }
+
+            if (results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
+                const landmarks = results.multiFaceLandmarks[0];
+
+                // 計算臉部的外接矩形範圍
+                let minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
+                for (const point of landmarks) {
+                    const x = point.x * $canvas.width;
+                    const y = point.y * $canvas.height;
+                    minX = Math.min(minX, x);
+                    minY = Math.min(minY, y);
+                    maxX = Math.max(maxX, x);
+                    maxY = Math.max(maxY, y);
+                }
+
+                // 計算臉部面積
+                const faceWidth = maxX - minX;
+                const faceHeight = maxY - minY;
+                const faceArea = faceWidth * faceHeight;
+
+                // 根據面積調整圖片大小，面積越大，頭越近
+                const scale = faceArea / (window.innerWidth * window.innerHeight);
+                // $('.text').text(scale);
+
+                // 計算頭頂的座標
+                const topOfHead = landmarks[10];
+                const x = topOfHead.x * $canvas.width;
+                const y = topOfHead.y * $canvas.height;
+
+                const imgW = faceWidth + (scale * 10);
+                // 根據比例繪製圖片
+                ctx.drawImage(img, x - imgW / 2, y - imgW, imgW, imgW);
+            }
 
             ctx.restore();
         });
